6a9d3cfb51b1044d185aa5f92d5cb35da3c7ffd4
Subasangeeth <142552707+subasangeeth@users.noreply.github.com>
2025-10-22 10:23:46 +0530

    Add files via upload

---
+++ springapp-jenkins/src/main/java/com/springapp/WebApp.java
@@ -0,0 +1,27 @@
+package com.springapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WebApp {
+	
+		
+		 @GetMapping("/")
+		    public ModelAndView homePage(Model model) {
+		       
+			 ModelAndView mv=new ModelAndView();
+			    
+			 
+			 	String version = "v2.5.1";
+			 	model.addAttribute("message", "Welcome to Spring Boot");
+		        model.addAttribute("version", version);
+		        mv.setViewName("index.html");
+		        return mv;
+		   
+	}
+
+}
